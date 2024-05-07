30a7668f2b7c47cb785444657d588bd55b9a6d36
haykay36 <akeemayinla@gmail.com>
2024-05-07 12:09:34 -0500

    rendering of arrayData-exercise 1.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import React from "react";
-import Content from "./components/content";
 import Header from "./components/header";
 import Total from "./components/total";
+//create a constant variable 'courses' and an array of parts of the courses
+const courses = "Half stack application development";
+const parts = [
+  { name: "Fundamentals of react", exercises: 10 },
+  { name: "Using props to pass data", exercises: 7 },
+  { name: "Using props to pass data", exercises: 14 },
+];
 function App() {
+  const arrayOfData = parts.map((item) => (
+    <div>
+      <p>
+        {item.name} {item.exercises}
+      </p>
+    </div>
+  ));
   return (
     <div>
-      <Header course="Half stack application development" />
-      <Content
-        part1="Fundamentals of react"
-        part2="Using props to pass data"
-        part3="State of a component"
-        exercises1="10"
-        exercises2="7"
-        exercises3="14"
-      />
+      <Header course={courses} />
+      <p>{arrayOfData}</p>
       <Total exercises1={10} exercises2={7} exercises3={14} />
     </div>
   );
